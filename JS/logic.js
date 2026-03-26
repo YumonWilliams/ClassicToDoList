50f6d5cb2f7ae0d9bb93bf4ae94ebed567382a95
yumonwilliams <caddilacwarlock@gmail.com>
2026-03-26 13:10:33 -0400

    Fianlly getting somewhere!

---
+++ JS/logic.js
@@ -0,0 +1,73 @@
+const addButton = document.getElementById("addButton");
+const clearButton = document.getElementById("clearButton");
+
+addButton.addEventListener("click", addEntry);
+clearButton.addEventListener("click", clearEntry);
+
+let listArray = [];
+
+function textBox(){
+    //Receives the text from the form. 
+    const textInput = document.getElementById('textInput');
+    textInput.value.trim();
+};
+
+function arrayList(){
+    //Takes the value of the form and stores it into the array. 
+    let newEntry = textInput.value.trim();
+    listArray.push(newEntry);
+};
+
+//contains functions to display user's text input as entries in the to-do list. 
+function displayArray() {
+    const container = document.getElementById('myList')
+
+    //Clears the list and prepares it for user input.
+    container.innerHTML = '';
+
+    for(let i = 0; i < listArray.length; i++){
+        container.innerHTML += `
+            <div class="field-row">
+                <input type="checkbox" id="entry${i}">
+                <label for="entry${i}">${listArray[i]}</label>
+                <button id="removeButton${i}" style="margin-left: auto">REMOVE</button>
+            </div>
+        `; 
+    }
+
+
+};
+
+function addEntry() {
+    //Calls the textbox function and the function that adds new entries to the list.
+    textBox();
+    arrayList();
+    //Clears the text box after selecting the add button.
+    textInput.value = '';
+
+    //console log the array.
+    console.log(listArray)
+
+    displayArray(); 
+    
+};
+
+
+// This needs to be able to remove an entry from the list of the array. Maybe assign the index to the checkbox somehow and remove when it is selected. 
+function clearEntry() {
+    // Remove entry from the array. 
+    // listArray.pop();
+    // displayArray()
+    // console.log(listArray)
+
+    let listArray = [];
+    displayArray();
+    
+};
+
+//Calculates the progress bars' value from the amount of completed tasks. The value will then need to dynamically update the css styling of the progress bar. 
+function progressBar() {
+
+};
+
+console.log(listArray);
